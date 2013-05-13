492995ee530b526c19fa623d66fa20c202888852
Yuta NAKAI <nak5124@live.jp>
2013-05-13 23:48:25 +0900

    version

--- version.sh
+++ version.sh
@@ -1,12 +1,17 @@
-#!/bin/sh
+#!/usr/bin/env bash
+BUILD_DIR=`pwd`/
 [ -n "$1" ] && cd $1
 
 git_version() {
 trap 'rm -f config.git-hash' EXIT
 git rev-list HEAD | sort > config.git-hash
 LOCALVER=`wc -l config.git-hash | awk '{print $1}'`
+BIT_DEPTH=`grep "X264_BIT_DEPTH" < ${BUILD_DIR}x264_config.h | awk '{print $3}'`
+BUILD_ARCH=`grep "ARCH=" < ${BUILD_DIR}config.mak | awk -F= '{print $2}'`
+BUILD_ARCH=`echo $BUILD_ARCH | tr "[A-Z]" "[a-z]"`
+LAVF=`grep "HAVE_LAVF" < ${BUILD_DIR}config.h | awk '{print $3}'`
 if [ $LOCALVER \> 1 ] ; then
-    VER=`git rev-list origin/master | sort | join config.git-hash - | wc -l | awk '{print $1}'`
+    VER=`git rev-list origin/plain | sort | join config.git-hash - | wc -l | awk '{print $1}'`
     VER_DIFF=$(($LOCALVER-$VER))
     echo "#define X264_REV $VER"
     echo "#define X264_REV_DIFF $VER_DIFF"
@@ -16,7 +21,11 @@ if [ $LOCALVER \> 1 ] ; then
     if git status | grep -q "modified:" ; then
         VER="${VER}M"
     fi
-    VER="$VER $(git rev-list HEAD -n 1 | cut -c 1-7)"
+    if [ $LAVF == 1 ] ; then
+        VER="${VER} [${BUILD_ARCH} ${BIT_DEPTH}bit-depth]"
+    else
+        VER="${VER} [lite ${BUILD_ARCH} ${BIT_DEPTH}bit-depth]"
+    fi
     VERSION=" r$VER"
 fi
 }
